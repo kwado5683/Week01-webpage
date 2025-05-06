dd96b5d33c6797181dd9915c3e0894511da5c1a3
Val <chikwadovalentine2@gmail.com>
2025-05-06 09:06:52 +0100

    features: added new pages and link functions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Week01-webpage
 This assignment is to utilize the different HTML and CSS tools we learnt this first week.
+
+I was able to utilize most html and css tools for this project. However, understanding DOM is still quite challenging. I was unable to get my javascript to work, hopefully I will have better understanding with a little more practice.
---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+// const newHeading= document.createElement('h1');
+// newHeading.textContent = "This is a Test";
+
+// const contentDiv = document.getElementById("tickets");
+
+// contentDiv.appendChild(newHeading);
+
+
+// document.body.appendChild(bat);
+
+const magicButton = document.createElement("button");
+
+magicButton.textContent = "click Me - it's Magic";
+
+document.getElementById("magic-box").appendChild(magicButton);
